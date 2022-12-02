ccf7680cb64f8d002b2bf61cb5fc4953de4a5e42
thaistavares <thais.tavares@gmail.com>
2022-12-02 13:52:53 -0300

    feat: curso assistido até a aula 25

--- costs/src/components/pages/Projects.js
+++ costs/src/components/pages/Projects.js
@@ -1,5 +1,22 @@
+import {useLocation} from 'react-router-dom';
+
+import Message from "../layout/Message";
+
 function Projects() {
-    return <h1>Projects</h1>
+
+    const location = useLocation();
+    let message = '';
+
+    if(location.state) {
+        message = location.state.message;
+    }
+
+    return(
+        <div>
+            <h1>Meus Projetos</h1>
+            {message && <Message type="success" msg={message}></Message>}
+        </div>
+    );
 }
 
 export default Projects;
